a2376531688291c8dd6ef3eab77f78990b7738f4
Guardiac - Watchdog <leanerpy@proton.me>
2024-12-23 19:55:36 +0200

    Create Configuration.lua

---
+++ Guardiac Loader/Config.cfg/Configuration.lua
@@ -0,0 +1,22 @@
+return {
+	MaxSpeed = 50,
+	CheckInterval = 0.1,
+	Permissions = {
+		Creators = {},
+		HeadAdmins = {},
+		Admins = {},
+		Moderators = {}
+	},
+	Detections = {
+		SpeedHack = true,
+		Flying = true,
+		Noclip = true,
+		Teleport = true
+	},
+	KickMessages = {
+		SpeedHack = "Speed hacking detected",
+		Flying = "Flying detected",
+		Noclip = "Noclip detected",
+		Teleport = "Illegal teleport detected"
+	}
+}
